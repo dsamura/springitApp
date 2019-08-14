84780361789b973ab23392e72021b61ec2fb7f4a
daniel.salifu <daniel.salifu@ubuntuafrika.com>
2019-08-14 15:24:08 +0000

    Adding some new files

--- README.md
+++ README.md
@@ -1,6 +1,4 @@
 Springit
 ===
-A paragraph that tells about the project
-Another paeagaraph 
+Welcome to the SpringIt Project
 
-This is some codes
---
+++ src/main/java/com/daniel/springit/model/Vote.java
@@ -0,0 +1,58 @@
+package com.daniel.springit.model;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import java.util.Objects;
+
+@Entity
+public class Vote {
+
+    @Id
+    @GeneratedValue
+    private Long Id;
+    private int vote;
+
+    //user
+    //link
+
+    public Vote() { }
+
+    public Long getId() {
+        return Id;
+    }
+
+    public void setId(Long id) {
+        Id = id;
+    }
+
+    public int getVote() {
+        return vote;
+    }
+
+    public void setVote(int vote) {
+        this.vote = vote;
+    }
+
+    @Override
+    public String toString() {
+        return "Vote{" +
+                "Id=" + Id +
+                ", vote=" + vote +
+                '}';
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Vote vote1 = (Vote) o;
+        return vote == vote1.vote &&
+                Id.equals(vote1.Id);
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(Id, vote);
+    }
+}
